8b6cac863f0da59f039c6acaaf21ba1758a22c02
hades0628 <51470379+hades0628@users.noreply.github.com>
2024-01-30 10:37:57 +0900

    Update Person.java

--- src/ex08/Person.java
+++ src/ex08/Person.java
@@ -2,14 +2,22 @@ package ex08;
 
 public class Person {
 	
+	//정적변수, 클래스변수
 	private static int age;
 	private static String name;
 	
+	//멤버변수 인스턴스변수
+	private String address;
+	
+	
+	//메소드 인스턴스 메소드
 	void showInfo() {
 		System.out.println("나이 : " + age);
 		System.out.println("이름 : " + name);
 	}
 	
+	
+	//정적메소드 클래스 메서도
 	static void staticShow() {
 		System.out.println("static 메소드");
 		System.out.println(age);
